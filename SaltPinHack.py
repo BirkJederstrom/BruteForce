434479cf9512f9b0ba065ea5f5b09290850496b7
BirkJederstrom <63236277+BirkJederstrom@users.noreply.github.com>
2026-02-08 11:33:29 +0100

    Fixed the Salt Pin Hack

--- SaltPinHack.py
+++ SaltPinHack.py
@@ -1,19 +1,22 @@
 import hashlib
 
-nonSaltHash = "93a82987d3cdd931cd785dc93fddd305de9b488a"
+#Below follows the quick construction of the salted pin hash, used during testing
+
+#pin = '7890'
+#salt1 = '1fa6'
+#h = hashlib.sha256((pin + salt1).encode()).hexdigest()
+
+h = "93a82987d3cdd931cd785dc93fddd305de9b488a"
+salt = '1fa6'
 x = 0
 
-salt = '1fa6'
+for x in range(9999):
+    D = ('{:04d}'.format(x) + salt)
+    #print(D)
 
-h = nonSaltHash + salt
-
-print(h)
-
-for x in range(9999999):
-    D = '{:04d}'.format(x)
-    print(D)
-
-    if hashlib.sha3_512(D.encode()).hexdigest() == h:
-        print("Found pass ", D)
+    if hashlib.sha256(D.encode()).hexdigest() == h:
+        print("Found pass + salt: ", D)
+        password = D.replace(salt, "")
+        print("The Password: ", password)
+        print("The salt was: ", salt)
         break
-
